b7729ba706e62a630a78af1248a3e00e5c65adf5
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-05 00:05:40 +0800

    update build
    update build

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar -Dserver.port=8761 service-registry-0.0.1-SNAPSHOT.jar
+sudo java -jar -Dserver.port=9191 api-gateway-0.0.1-SNAPSHOT.jar
+sudo java -jar -Dserver.port=4502 portal-service-0.0.1-SNAPSHOT.jar
+sudo java -jar -Dserver.port=9001 listing-service-0.0.1-SNAPSHOT.jar
+sudo java -jar -Dserver.port=9002 user-service-0.0.1-SNAPSHOT.jar
