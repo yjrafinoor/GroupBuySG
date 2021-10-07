e7a3aba88504c7a97dded3384699c8011b89da33
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-07 11:57:03 +0800

    trigger multiple build

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar &
+sleep 30
 sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar &
+sleep 30
 sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar &
+sleep 30
 sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar &
+sleep 30
 sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar &
