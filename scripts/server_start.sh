e5af8c847a40a10fa6506993a96cbeb2136d7bdc
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-07 11:08:19 +0800

    deployment script

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar service-registry-0.0.1-SNAPSHOT.jar &
-sudo java -jar api-gateway-0.0.1-SNAPSHOT.jar &
-sudo java -jar portal-service-0.0.1-SNAPSHOT.jar &
-sudo java -jar listing-service-0.0.1-SNAPSHOT.jar &
-sudo java -jar user-service-0.0.1-SNAPSHOT.jar &
+sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar &
+sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar &
+sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar &
+sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar &
+sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar &
