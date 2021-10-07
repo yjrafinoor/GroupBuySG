4d6e35973a22d86032a6572b073a9adac748b05f
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-07 11:46:38 +0800

    script fix

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/service-registry-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/server/api-gateway-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/server/portal-service-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/server/listing-service-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/server/user-service-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+sudo chmod +x /home/ec2-user/server/service-registry-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/api-gateway-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/portal-service-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/listing-service-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/user-service-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/server_start.sh
+sudo chmod +x /home/ec2-user/server/server_stop.sh
