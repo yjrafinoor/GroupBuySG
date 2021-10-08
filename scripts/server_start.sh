d9ff39f4f403023537b67d948ffe0fb1664b0bac
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-08 09:39:40 +0800

    single jar file deployment

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,11 +2,11 @@
 cd /home/ec2-user/server
 sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar &
 sleep 30
-sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar &
-sleep 30
-sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar &
-sleep 30
-sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar &
-sleep 30
-sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar &
-sleep 30
+#sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar &
+#sleep 30
+#sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar &
+#sleep 30
+#sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar &
+#sleep 30
+#sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar &
+#sleep 30
