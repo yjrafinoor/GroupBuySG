ff60f0a6e2faa01bae477e25cf0ea99be6f5231d
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-08 10:12:11 +0800

    fix start deployment script

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar &
-sleep 30
+sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+#sleep 30 &&
 #sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar &
 #sleep 30
 #sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar &
