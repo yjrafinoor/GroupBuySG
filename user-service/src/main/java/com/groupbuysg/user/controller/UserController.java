c60c46540d99dc9924836e3f4ea30b3ef0f59f03
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-08 14:19:05 +0800

    testing rest api

--- user-service/src/main/java/com/groupbuysg/user/controller/UserController.java
+++ user-service/src/main/java/com/groupbuysg/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.groupbuysg.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,11 @@ public class UserController {
 		log.info("Inside getUserWithListing method of UserController");
 		return userService.getUserWithListing(UserId);
 	}
+
+	  @GetMapping("/health")
+    public ResponseEntity<?> healthCheck()
+    {
+        return ResponseEntity.ok("Controller works in aws");
+    }
 	
 }
