16895e8536f126a95a9319174b2595985fd6baa9
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-08 10:49:49 +0800

    fix deploy script

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-sudo chmod +x /home/ec2-user/server/service-registry-0.0.1-SNAPSHOT.jar
-sudo chmod +x /home/ec2-user/server/api-gateway-0.0.1-SNAPSHOT.jar
-sudo chmod +x /home/ec2-user/server/portal-service-0.0.1-SNAPSHOT.jar
-sudo chmod +x /home/ec2-user/server/listing-service-0.0.1-SNAPSHOT.jar
-sudo chmod +x /home/ec2-user/server/user-service-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ec2-user/server/* jar
 sudo chmod +x /home/ec2-user/server/server_start.sh
 sudo chmod +x /home/ec2-user/server/server_stop.sh
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 sleep 10 
-sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 sleep 10
-sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 sleep 10
-sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 sleep 10
-sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
