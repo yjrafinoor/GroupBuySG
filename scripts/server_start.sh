23270fb6b11cb63adee2b0380490a3a3345c995a
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-15 15:15:02 +0800

    server start test

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-#sleep 10 
-#sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-#sleep 10
-#sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-#sleep 10
-#sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-#sleep 10
-#sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > service.out 2>&1 &
+sleep 10 
+sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > gateway.out 2>&1 &
+sleep 10
+sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > portal.out 2>&1 &
+sleep 10
+sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar> listing.out 2>&1 &
+sleep 10
+sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > user.out 2>&1 &
