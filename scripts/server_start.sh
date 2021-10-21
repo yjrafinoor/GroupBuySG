164d43478aac9935ae52c0c46efae82eb764430f
yjrafinoor <yj.rafinoor@ncs.com.sg>
2021-10-21 12:12:28 +0800

    code cleanup

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,10 +2,10 @@
 cd /home/ec2-user/server
 sudo nohup java -jar service-registry-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 #sleep 10 
-#sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar api-gateway-0.0.1-SNAPSHOT.jar > gateway.out 2>&1 &
 #sleep 10
-#sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar portal-service-0.0.1-SNAPSHOT.jar > portal.out 2>&1 &
 #sleep 10
-#sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar listing-service-0.0.1-SNAPSHOT.jar > listing.out 2>&1 &
 #sleep 10
-#sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar user-service-0.0.1-SNAPSHOT.jar > user.out 2>&1 &
